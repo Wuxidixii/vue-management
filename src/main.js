c489984b57ec5a9317b428fca1c94934401b69eb
wuxidixi <wuxidixi@xx.com>
2022-03-12 13:56:34 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Container, Header, Main, Aside } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container style="height: 100%">
+    <el-aside width="auto">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang='less' scoped>
+.el-header {
+  background-color: #333;
+}
+.el-main {
+  padding-top: 0;
+}
+</style>
